16c33cc06aa61b1a0d9b780eb7f8807da9f3a1ac
Apoorva Pendse <apoorvavpendse@gmail.com>
2024-08-20 15:35:05 +0530

    use env vars instead of hardcoded vals

--- redis/redisFuncs.js
+++ redis/redisFuncs.js
@@ -1,9 +1,10 @@
 import Redis from "ioredis";
-
+import dotnev from 'dotenv'
+dotnev.config();
 const redis = new Redis({
-  port: 17156,
-  host: 'redis-17156.c321.us-east-1-2.ec2.cloud.redislabs.com',
-  password: 'sO3b4FzLLPQHHSj8LzVynYg4ECA0tb2C'
+  port: process.env.REDIS_PORT,
+  host: process.env.REDIS_HOST,
+  password: process.env.REDIS_PASS
 });
 
 // creats a game in GlobalHashMap with host's mail
